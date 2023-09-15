9b72d4c592e48bd4459a82e5a18f038035f85171
Lukasz Ortman <ortman.luk@gmail.com>
2023-09-15 21:18:40 +0200

    Product tests

---
+++ jakTestowacDemoTest.py
@@ -0,0 +1,135 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+from helpers import functional_helpers
+from helpers import invalid_module
+
+
+"""
+class ProductTests(unittest.TestCase):
+
+   def setUp(self):
+       self.driver = webdriver.Chrome(service=Service(r'/Users/macbookpro13/Desktop/GoogleChromeforTesting.app'))
+       self.url = 'https://autodemo.testoneo.com/en/login?back=my-account'
+
+
+   def tearDown(self):
+       self.driver.quit()
+
+   def assert_elemnt_text(self,driver, xpath, expected_text):
+       self.header = driver.find_element(By.XPATH, xpath )
+       header_text = self.header.text
+       self.assertEqual(expected_text, header_text,
+                        f'Expected login button text: log in to your account , differ from actual {header_text}')
+
+   def test_checking_header_is_log_in_to_your_account(self):
+       driver = self.driver
+       driver.get(self.url)
+       self.assert_elemnt_text(driver, '//*[@id="main"]/header/h1', expected_text='Log in to your account')
+       time.sleep(2)
+"""
+
+#metoda user_correct_login
+class ProductTests(unittest.TestCase):
+
+   def setUp(self):
+       self.driver = webdriver.Chrome(service=Service(r'/Users/macbookpro13/Desktop/GoogleChromeforTesting.app'))
+       self.login_url = 'https://autodemo.testoneo.com/en/login?back=my-account'
+       self.user_mail = 'ortman@op.pl'
+       self.user_password = 'hiphop00'
+
+   def tearDown(self):
+       self.driver.quit()
+
+   def test_user_login(self):
+
+       driver=self.driver
+       driver.get(self.login_url)
+       functional_helpers.user_login(driver, self.user_mail, self.user_password)
+
+
+
+
+
+
+
+
+
+"""
+class ProductTests(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome(service=Service(r'/Users/macbookpro13/Desktop/GoogleChromeforTesting.app'))
+        self.url = 'https://autodemo.testoneo.com/en/login?back=my-account'
+        self.fail_email = 'ortma@op.pl'
+        self.fail_password = 'hipho00'
+        self.fail_alert = 'Authentication failed.'
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def fill_input(self, driver, xpath, value):
+        self.input_box = driver.find_element(By.XPATH, xpath)
+        self.input_box.clear()
+        self.input_box.send_keys(value)
+
+    def test_sign_in_using_fake_email_and_password(self):
+        driver = self.driver
+        driver.get(self.url)
+
+        # finding password box 1 and fill in faild email
+        self.fill_input(self.driver, '//*[@id="login-form"]/section/div[1]/div[1]/input', self.fail_email)
+
+        # finding password box 2 and fill in faild password
+        self.fill_input(self.driver, '//*[@id="login-form"]/section/div[2]/div[1]/div/input', self.fail_password)
+
+        sign_in_button = driver.find_element(By.XPATH, '//*[@id="submit-login"]')
+        sign_in_button.click()
+        time.sleep(2)
+
+        # checking alert information
+        faild_sign_alert = driver.find_element(By.XPATH, '//*[@id="content"]/section/div/ul/li')
+        faild_sign_alert_text = faild_sign_alert.text
+        self.assertEqual(self.fail_alert, faild_sign_alert_text,
+                         f"If you don't see this message, probably your email and password are correct")
+
+        time.sleep(3)
+"""
+
+
+
+
+""""
+class ProductTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        self.driver = webdriver.Chrome(service=Service(r'/Users/macbookpro13/Desktop/GoogleChromeforTesting.app'))
+        self.url = 'https://autodemo.testoneo.com/en/men/1-1-hummingbird-printed-t-shirt.html'
+
+    @classmethod
+    def tearDownClass(self):
+        self.driver.quit()
+
+
+    def test_checking_product_on_website(self):
+        driver = self.driver
+        driver.get(self.url)
+        expected_tshirt_name='HUMMINGBIRD PRINTED T-SHIRT'
+        tshirt_name = driver.find_element(By.XPATH, '//*[@id="main"]/div[1]/div[2]/h1')
+        tshirt_name_text = tshirt_name.text
+        self.assertEqual(expected_tshirt_name, tshirt_name_text,
+                         f'Expected tshirt name: HUMMINGBIRD PRINTED T-SHIRT , differ from actual {tshirt_name_text}')
+        time.sleep(2)
+
+    def test_checking_product_price_on_website(self):
+        driver = self.driver
+        driver.get(self.url)
+        expected_tshirt_price='PLN23.52'
+        tshirt_price = driver.find_element(By.XPATH, '//*[@id="main"]/div[1]/div[2]/div[1]/div[2]/div/span[1]')
+        tshirt_price_text = tshirt_price.text
+        self.assertEqual(expected_tshirt_price, tshirt_price_text,
+                         f'Expected tshirt price: PLN23.52 , differ from actual {tshirt_price_text}')
+        time.sleep(2)
+"""
