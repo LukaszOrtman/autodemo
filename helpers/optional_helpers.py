ba6eb47dcf1375fc7118d6657c86b3db38269220
Lukasz Ortman <ortman.luk@gmail.com>
2023-09-19 21:15:09 +0200

    Add locator and alement_located in def visibility_of_element_wait

--- helpers/optional_helpers.py
+++ helpers/optional_helpers.py
@@ -1,6 +1,9 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
 import time
 from selenium.webdriver.common.by import By
-
 
 
 def wait_for_elements(driver, xpath, max_seconds_to_wait=5, number_of_expected_elements=1):
@@ -20,6 +23,23 @@ def wait_for_elements(driver, xpath, max_seconds_to_wait=5, number_of_expected_e
 
         if seconds == (max_seconds_to_wait - 1):
             print('End of wait')
-            assert len(elements) > number_of_expected_elements, f'Expected {number_of_expected_elements} elements but found {len(elements)} for xpath {xpath} in time of {max_seconds_to_wait}s.'
+            assert len(
+                elements) > number_of_expected_elements, f'Expected {number_of_expected_elements} elements but found {len(elements)} for xpath {xpath} in time of {max_seconds_to_wait}s.'
 
         time.sleep(2)
+
+
+def visibility_of_element_wait(driver, xpath, timeout=10):
+    """Checking if element specified by xpath is visible on page
+
+    :param driver: webdriver instance
+    :param xpath: xpath of web element
+    :param timeout: time after looking for element will be stopped (timeout= 10)
+    :return: found element
+    """
+    timeout_message = f"Element for xpath: '{xpath}' and url: {driver.current_url} not found in {timeout} seconds. "
+
+    locator = (By.XPATH, xpath)
+    element_located = EC.visibility_of_element_located(locator)
+    return WebDriverWait(driver, timeout).until(element_located, timeout_message)
+
